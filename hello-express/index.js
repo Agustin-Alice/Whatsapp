4e264be7dc8204301f814eebdfd5ab47d77fbef8
Agustin Alice <agustin_alice@Agustins-MacBook-Pro.local>
2022-10-12 19:24:01 -0300

    fix endpoint creater message

--- hello-express/index.js
+++ hello-express/index.js
@@ -2,38 +2,43 @@ const express = require("express");
 const { Client } = require("pg");
 //fix eslintrc
 const client = new Client({
-  host: "localhost:3001",
-  port: 3001,
-  user: "admin",
-  password: "password",
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "elogse04dn",
 });
+
+let messages = [
+  {
+    id: 1,
+    text: "Hello Word",
+    creator_by_id: 2,
+    channel_id: 5,
+    created_at: " ", // aca iria la hora o el dia?
+  },
+];
 
 client
   .connect()
   .then(() => console.log("connected"))
+  .then(() => {
+    const app = express();
+    const PORT = 3002;
+
+    app.use(express.json());
+
+    // app.get("/api/messages/:id", (req, res) => {});
+
+    app.post("/api/messages", (req, res) => {
+      const message = req.body;
+      res.json(message);
+    });
+
+    app.get("/", (req, res) => {
+      // recibe json responde con json
+      res.json(messages);
+    });
+
+    app.listen(PORT, () => console.log("server is listening on port " + PORT));
+  })
   .catch((err) => console.error("connection error", err.stack));
-
-const app = express();
-const PORT = 3002;
-
-let messages = [
-  {
-    id: 0,
-    text: "Hello Word",
-    creator_by_id: 2,
-    channel_id: 5,
-    created_at: 54,
-  },
-];
-app.use(express.json());
-
-app.post("/api/messages", (req, res) => {
-  const message = req.body;
-  res.json(message);
-});
-app.get("/api/messages", (req, res) => {
-  // recibe json responde con json
-  res.json(messages);
-});
-
-app.listen(PORT, () => console.log("server is listening on port " + PORT));
